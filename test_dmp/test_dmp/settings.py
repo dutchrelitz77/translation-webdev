e2b476ebb2f23d8245ef6bde1a20c3f6f19256be
Justin R <justin.relitz@gmail.com>
2015-03-24 12:52:05 -0600

    Changed to a postgreSQL database

--- test_dmp/test_dmp/settings.py
+++ test_dmp/test_dmp/settings.py
@@ -88,8 +88,12 @@ LOGGING = {
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', 
+        'NAME': 'exampledb',                     
+        'USER': 'postgres',
+        'PASSWORD': 'byurocks77',
+        'HOST': 'localhost',   
+        'PORT': '5432',                    
     }
 }
 
